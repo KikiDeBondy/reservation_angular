bea538d65e134df0006c8cc16b0abf2e7bd69c22
KikiDeBondy <killian49@hotmail.fr>
2025-01-30 16:19:33 +0100

    msg error

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,13 +2,15 @@ import {Component, inject, ViewChild} from '@angular/core';
 import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
 import {Router, RouterLink} from "@angular/router";
 import {AuthentificationService} from "../../Services/auth/authentification.service";
+import {AlertComponent} from "@coreui/angular";
 
 @Component({
   selector: 'app-register',
   imports: [
     FormsModule,
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
+    AlertComponent
   ],
   templateUrl: './register.component.html',
   standalone: true,
@@ -16,14 +18,21 @@ import {AuthentificationService} from "../../Services/auth/authentification.serv
 })
 export class RegisterComponent {
 
+  errorMessage: string | null = null;
+
   private authService = inject(AuthentificationService);
   private router = inject(Router);
   @ViewChild('registerForm') registerForm!: NgForm;
 
   onSubmit() {
     console.log(this.registerForm.value);
-    // this.authService.registration(this.registerForm.value).subscribe({
-    //
-    // })
+    this.authService.registration(this.registerForm.value).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        this.errorMessage = err.error.error
+      }
+    })
   }
 }
